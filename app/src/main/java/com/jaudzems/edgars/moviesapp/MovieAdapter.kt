e6906025ebdfd95b01bc7dc2c6e565ad7e6ca6f3
“EdgarsJaudzems” <jaudzems.edgars@inbox.lv>
2021-05-24 15:46:20 +0300

    Glide + image load

--- app/src/main/java/com/jaudzems/edgars/moviesapp/MovieAdapter.kt
+++ app/src/main/java/com/jaudzems/edgars/moviesapp/MovieAdapter.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class MovieAdapter(
     val context: Context,
@@ -13,8 +15,18 @@ class MovieAdapter(
 ): RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        private val IMAGE_BASE = "https://image.tmdb.org/t/p/w500/"
+
         var title = itemView.findViewById<TextView>(R.id.movie_title)
         var releaseDate = itemView.findViewById<TextView>(R.id.movie_release_date)
+        var poster = itemView.findViewById<ImageView>(R.id.movie_image)
+
+        fun bind(movie: Result) {
+            title.text = movie.title
+            releaseDate.text = movie.release_date
+            Glide.with(itemView).load(IMAGE_BASE + movie.poster_path).into(poster)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -23,8 +35,9 @@ class MovieAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.title.text = movieList[position].title
-        holder.releaseDate.text = movieList[position].release_date
+        return holder.bind(movieList[position])
+//        holder.title.text = movieList[position].title
+//        holder.releaseDate.text = movieList[position].release_date
     }
 
     override fun getItemCount(): Int {
